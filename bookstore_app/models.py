0f864e519d74c178a1b63f8f5f543a3c56f28404
r4ghuveer <raghuveerofficial08@gmail.com>
2024-01-26 15:10:58 +0530

    -

--- bookstore_app/models.py
+++ bookstore_app/models.py
@@ -1,7 +1,7 @@
 # bookstore/models.py
 from django.db import models
 
-class Book(models.Model):
+class Book(models.Model): #data model
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
